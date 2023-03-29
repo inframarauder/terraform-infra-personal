8f2ede05899653990e1a72def26f8e0fbffec077
subhasis020299 <subhasisdas125@gmail.com>
2023-03-30 02:27:23 +0530

    added authkey

--- tailscale/main.tf
+++ tailscale/main.tf
@@ -2,3 +2,11 @@
 resource "tailscale_acl" "acl" {
   acl = file("${path.module}/files/acl.json")
 }
+
+# Tailscale authkeys - (for use with tailscale up --authkey)
+resource "tailscale_tailnet_key" "exit_node" {
+  reusable      = true
+  ephemeral     = true
+  preauthorized = true
+  tags          = toset(["tag:exit-node", "tag:aws-ec2"])
+}
---
+++ tailscale/outputs.tf
@@ -0,0 +1,3 @@
+output "tailscale_authkey_exit_node" {
+  value = tailscale_tailnet_key.exit_node.key
+}
