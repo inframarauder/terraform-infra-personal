9ec45d3d0e9147bfb454bf64c4e43768e9433da0
subhasis020299 <subhasisdas125@gmail.com>
2023-04-02 19:58:24 +0530

    added provider block and variables

---
+++ aws-ec2-self-hosted-vpn/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  type        = string
+  description = "The AWS region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  type        = string
+  description = "The AWS_ACCESS_KEY_ID for the terraform user"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "The AWS_SECRET_ACCESS_KEY for the terraform user"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The instance type to use for the VPN server"
+  default     = "t4g.nano"
+}
