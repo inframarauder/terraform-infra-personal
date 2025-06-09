49fb931149e83473d70feaa8213c4f40091c880d
subhasis020299 <subhasisdas125@gmail.com>
2025-06-10 02:19:38 +0530

    added eks cluster for ai experiments

---
+++ eks-cluster-ai/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.9.0"
+
+    }
+
+
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
---
+++ eks-cluster-ai/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+  default     = "ap-south-1"
+}
+
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "Name of the EKS cluster"
+  default     = "eks-cluster-ai"
+}
+
+variable "eks_cluster_version" {
+  type        = string
+  description = "Version of the EKS cluster"
+  default     = "1.33"
+}
